8f0b66eeecc519785853361907ccca17c71568f5
Said Belwahed <saidbelwahed@gmail.com>
2023-05-19 01:31:32 +0100

    tasks 0 - 6, 100, 101

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints an exact string
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);/* returns 0 */
+}
---
+++ 5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints an exact string followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
