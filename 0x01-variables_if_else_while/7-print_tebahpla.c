ebf78bf3fa2620acb65f2e98159ed66a6ccf8712
said belowahed <saidbelwahed@gmail.com>
2023-05-19 17:37:40 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabet in lowercase and reverse,
+ * i
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
