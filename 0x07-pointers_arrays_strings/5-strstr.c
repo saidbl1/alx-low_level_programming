9da4b10de2a25c5212123a6095bbabb327e7b3f5
Said Belwahed <saidbelwahed@gmail.com>
2023-06-05 20:11:34 +0100

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * _strstr - locates a substring in a string
+ * @haystack: pointer to input string
+ * @needle: pointer to substring to search for
+ *
+ * Return: pointer to the beginning of the located substring, or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+### 0x07. C - Even more pointers, arrays and strings
+
+## Learning Objectives
+
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google.
+
+
